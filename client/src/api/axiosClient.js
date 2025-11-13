ff6df378794a088bd14400e3218338f9e168aebc
SebastianFV <debastian614@gmail.com>
2025-11-13 16:15:29 -0500

    Problemas con el front

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,9 +1,12 @@
+// client/src/api/axiosClient.js
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("🔥 API URL cargada en producción:", API_URL);
+
 const axiosClient = axios.create({
-  baseURL: API_URL, 
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
